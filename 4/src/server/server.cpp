649eadeef9c4148a9c0f0c4af135f8b30527186e
ew22g22 <ew22g22@soton.ac.uk>
2022-10-25 05:23:31 +0100

    Added some comments

--- 4/src/server/server.cpp
+++ 4/src/server/server.cpp
@@ -3,16 +3,21 @@
 
 Server::Server()
 {
+  // Start up WinSock if on windows
   NetTCPSocket::StartupSockets();
 
+  // Initialize NetServer instance
   this->m_raw_server = std::make_shared<NetServer>(52000);
 
+  // Start the server and enter the loop on a different thread
+  // (leave main thread open for commands)
   this->m_raw_server->StartServer();
   this->m_raw_server->EnterLoop();
 }
 
 Server::~Server()
 {
+  // Shut down WinSock if on windows
   NetTCPSocket::ShutdownSockets();
 }
 
--- 4/src/server/server.hpp
+++ 4/src/server/server.hpp
@@ -3,15 +3,16 @@
 
 #include "../net/net.hpp"
 
+/* Wrapper class around NetServer for ease of use */
 class Server
 {
-  std::shared_ptr<NetServer> m_raw_server = nullptr;
+  std::shared_ptr<NetServer> m_raw_server = nullptr;    // Underlying NetServer instance
 
 public:
   Server();
   ~Server();
 
-  std::shared_ptr<NetServerEvents> GetEvents() const;
+  std::shared_ptr<NetServerEvents> GetEvents() const;   // Return the NetServerEvents structure from the NetServer instance
 };
 
 #endif
